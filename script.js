75d0214246d6a6d4e12c6d970750ea2bb292937d
Jonas Silva <90778653+jonas-amilton@users.noreply.github.com>
2023-02-26 20:45:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//typeof serve para dizer o tipo da variavel
+console.log(typeof 12)
+
+
+//Operador resto da divisão: %
+console.log(20 % 2)
+//resultado de 20/2 = 0
+
+//Operador de multiplicação: *
+console.log(2 * 2)
+//resultado 4
+
+//Operador de divisão: /
+console.log(4 / 2)
+//resultado 2
+
+//Operador de soma: +
+console.log(2 + 1)
+//resultado 3
+
+//Operador de subtração: -
+console.log(5 - 4)
+//resultado 1
